755cf57b4eb4293e10a3dd5b70dba4635862816e
fabianoteixeira <fabianotxss@gmail.com>
2019-06-25 23:51:48 -0300

    commit

---
+++ MvcClient/Startup.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MvcClient
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = "MyMvcApp";
+                options.DefaultChallengeScheme = "oidc";
+            })
+            .AddCookie("MyMvcApp")
+            .AddOpenIdConnect("oidc", options => {
+                options.SignInScheme = "MyMvcApp";
+                options.RequireHttpsMetadata = false;
+                options.Authority = "http://localhost:5000";
+                options.ClientId = "mvc.implicit";
+                options.ResponseType = "token id_token";
+                options.SaveTokens = true;
+                options.Scope.Add("NotaFiscal");
+            })
+            ;
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseAuthentication();
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
